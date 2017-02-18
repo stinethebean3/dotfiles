a116e3a7b60e2cc4e00eeea4e5b2aff8dd692c10
Christine Vick <cdevlin23.cd@gmail.com>
2017-02-17 19:44:17 -0800

    updated awesome config to use nitrogen wallpaper

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -38,7 +38,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
+beautiful.init(os.getenv("HOME").."/.config/awesome/themes/default/theme.lua")
+
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvtc"
